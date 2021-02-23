d888eeca9750600deb46ffe2be05609e4e02b4b4
marco.crettena <marco.crettenand@hevs.ch>
2021-02-23 13:43:54 +0100

    New version of App.tsx to test the new gateway
    Show a little sample of how to use basically the new interfaces

--- src/App.tsx
+++ src/App.tsx
@@ -3,153 +3,88 @@ import './App.css';
 
 import logo from "./logo-studer.png"
 
-import SIAbstractGatewayClient, {SIDescriptionFlags} from "./SIAbstractGatewayClient"
-import {Response} from "./SIAbstractGatewayClient"
+import {SIConnectionState, SIDeviceMessage} from "./SIGatewayClient"
 
 import Devices from "./Devices"
 import DeviceRender from "./DeviceRender";
+import OpenStuderInterface from "./OpenStuderInterface";
 
-enum SIConnectionState{
-  DISCONNECTED,
-  CONNECTING,
-  AUTHORIZING,
-  CONNECTED,
+let oui;
+
+class WebClientEnumerateTest extends OpenStuderInterface{
+  callbackProvider:App;
+  public constructor(callbackProvider:App, host:string,port:number=1987, user?:string, password?:string) {
+      super(host,port,user,password);
+      this.callbackProvider=callbackProvider;
+  }
+
+  public onEnumerateCallback(deviceMessage: SIDeviceMessage) {
+      super.onEnumerateCallback(deviceMessage);
+      if(deviceMessage.deviceCount) {
+          this.callbackProvider.enumerateCallback(deviceMessage.deviceCount);
+      }
+  }
+
+  public onChangeConnectionState(state: SIConnectionState) {
+      super.onChangeConnectionState(state);
+      if(state===SIConnectionState.CONNECTED){
+          this.enumerate();
+      }
+  }
 }
 
-type AppProps={
+class WebClientReadTest extends OpenStuderInterface{
+  callbackProvider:App;
+  public constructor(callbackProvider:App, host:string,port:number=1987, user?:string, password?:string) {
+    super(host,port,user,password);
+    this.callbackProvider=callbackProvider;
+  }
 
+  public onPropertyReadCallback(deviceMessage: SIDeviceMessage) {
+      super.onPropertyReadCallback(deviceMessage);
+    if(deviceMessage.value) {
+      this.callbackProvider.readPropertyCallback(deviceMessage.value);
+    }
+  }
+
+  public onChangeConnectionState(state: SIConnectionState) {
+    super.onChangeConnectionState(state);
+    if(state===SIConnectionState.CONNECTED){
+      this.readProperty("xcom.10.3023");
+    }
+  }
 }
 
 type AppState={
   testAuthorize:string,
   testEnumerate:string,
   testRead:string,
-  testDescription:string,
-  devices:Devices,
-  stateSI:SIConnectionState;
 }
 
-let ws:WebSocket;
-
-class App extends React.Component<AppProps, AppState> {
-  //SINGLETON PATTERN
-  private static appInstance:App;
-  public static getInstance():App{
-    if(!App.appInstance){
-      App.appInstance= new App({});
-    }
-    return App.appInstance;
-  }
-
+class App extends React.Component<{ }, AppState> {
+  wcet:WebClientEnumerateTest;
+  wcrt:WebClientReadTest;
   constructor(props:any) {
     super(props);
-    this.state={testAuthorize:"-",testEnumerate:"-",testRead:"-",testDescription:"-",
-      devices:new Devices(),stateSI:SIConnectionState.DISCONNECTED};
+    this.state={testAuthorize:"-",testEnumerate:"-",testRead:"-"};
+    this.wcet = new WebClientEnumerateTest(this,'ws://153.109.24.113',1987, "installer","installer");
+    this.wcrt = new WebClientReadTest(this,'ws://153.109.24.113',1987, "installer","installer");
   }
 
-  public componentDidMount(){
-    ws=new WebSocket('ws://153.109.24.113:1987');
-    this.setStateSI(SIConnectionState.CONNECTING);
-    let frame:string;
-    ws.onmessage = (evt: MessageEvent)=>{
-      let command:string = SIAbstractGatewayClient.peek_frame_command(evt.data);
-      this.setState({testDescription:evt.data});
-      switch(command){
-        case "AUTHORIZED":
-          this.setStateSI(SIConnectionState.CONNECTED);
-          this.setState({testAuthorize:""+SIAbstractGatewayClient.decode_authorized_frame(evt.data)});
-          frame = SIAbstractGatewayClient.encode_enumerate_frame();
-          ws.send(frame);
-        break;
-        case "ENUMERATED":
-          this.setState({testEnumerate:""+SIAbstractGatewayClient.decode_enumerate_frame(evt.data).device_count});
-          let flags:SIDescriptionFlags[]=[SIDescriptionFlags.INCLUDE_PROPERTY_INFORMATION,SIDescriptionFlags.INCLUDE_ACCESS_INFORMATION,
-          SIDescriptionFlags.INCLUDE_DEVICE_INFORMATION,SIDescriptionFlags.INCLUDE_DRIVER_INFORMATION];
-          frame = SIAbstractGatewayClient.encode_describe_frame(undefined,undefined,flags);
-          ws.send(frame);
-          break;
-        case "PROPERTY READ":
-          let responseProperty:Response = SIAbstractGatewayClient.decode_property_read_frame(evt.data);
-          this.setState({testRead:""+responseProperty.value});
-          break;
-        case "DESCRIPTION":
-          let responseDescription:Response = SIAbstractGatewayClient.decode_description_frame(evt.data);
-          let devicesTemp:Devices=new Devices();
-          devicesTemp.jsonToDevices(""+responseDescription.description);
-          this.setState({testDescription:""+responseDescription.description,devices:devicesTemp});
-          frame= SIAbstractGatewayClient.encode_subscribe_property_frame("xcom.11.3000");
-          ws.send(frame);
-          break;
-        case "PROPERTY SUBSCRIBED":
-          let responseSubscription:Response = SIAbstractGatewayClient.decode_property_subscribed_frame(evt.data);
-          break;
-      }
-    };
-    ws.onopen = function (event) {
-      let frame = SIAbstractGatewayClient.encode_authorize_frame("expert","expert");
-      App.getInstance().setStateSI(SIConnectionState.AUTHORIZING);
-      ws.send(frame);
-    };
+  public enumerateCallback(deviceCount:string){
+    this.setState({testEnumerate:deviceCount});
   }
 
-  public setStateSI(state:SIConnectionState){
-    this.setState({stateSI:state});
+  public readPropertyCallback(value:string){
+      this.setState({testRead:value});
   }
 
-  public onClick(){
-    let frame = SIAbstractGatewayClient.encode_read_property_frame("xcom.10.3023");
-    ws.send(frame);
+  public componentDidMount() {
+    this.wcet.connect();
+    this.wcrt.connect();
   }
 
-  public onSubscribeClick(){
-
-  }
-
-  public renderConnecting(){
-    return(
-        <div className="App">
-          <header className="App-header">
-            <h1 className="Title">
-              <div><img src={logo} alt="" className="App-logo"/><span className="marge">StuderNext</span></div>
-            </h1>
-            <p>Connecting...</p>
-          </header>
-        </div>
-    );
-  }
-
-  public renderDisconnected(){
-    return(
-        <div className="App">
-          <header className="App-header">
-            <h1 className="Title">
-              <div><img src={logo} alt="" className="App-logo"/><span className="marge">StuderNext</span></div>
-            </h1>
-            <p>Disconnected.</p>
-          </header>
-        </div>
-    );
-  }
-
-  public renderAuthorizing(){
-    return(
-        <div className="App">
-          <header className="App-header">
-            <h1 className="Title">
-              <div><img src={logo} alt="" className="App-logo"/><span className="marge">StuderNext</span></div>
-            </h1>
-            <p>Authorizing...</p>
-          </header>
-        </div>
-    );
-  }
-
-  public renderConnected(){
-    const deviceRender = this.state.devices.devices.map(device =>{
-      return(
-          <DeviceRender device={device} subscribeStatus={false} onClick={this.onSubscribeClick}/>
-      );
-    });
+  public render() {
     return (
         <div className="App">
           <header className="App-header">
@@ -158,42 +93,10 @@ class App extends React.Component<AppProps, AppState> {
             </h1>
             <p>Protocol version : {this.state.testAuthorize}</p>
             <p>Device count : {this.state.testEnumerate}</p>
-            <button onClick={()=>this.onClick()}>Read command</button>
             <p>Read value : {this.state.testRead}</p>
-            <p>Last message is : {this.state.testDescription}</p>
-            <p>{deviceRender}</p>
           </header>
         </div>
     );
-  }
-
-  public render() {
-    switch (this.state.stateSI){
-      case SIConnectionState.DISCONNECTED:
-        return(
-            <div>
-              {this.renderDisconnected()}
-            </div>
-        );
-      case SIConnectionState.CONNECTING:
-        return(
-            <div>
-              {this.renderConnecting()}
-            </div>
-        );
-      case SIConnectionState.AUTHORIZING:
-        return(
-            <div>
-              {this.renderAuthorizing()}
-            </div>
-        );
-      case SIConnectionState.CONNECTED:
-        return(
-            <div>
-              {this.renderConnected()}
-            </div>
-        );
-    }
   }
 }
 
